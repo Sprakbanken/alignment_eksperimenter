7c8e2a501635ee149ea15c3d5df6d101a6111926
tita <tita.enstad@nb.no>
2024-12-03 10:10:41 +0100

    Omstrukturer utils

---
+++ src/align_documents/types.py
@@ -0,0 +1,5 @@
+from typing import TypedDict
+
+class Match(TypedDict):
+    corpus_id: int
+    score: float
---
+++ src/align_documents/utils/__init__.py
@@ -0,0 +1,2 @@
+import align_documents.utils.print as print
+import align_documents.utils.split as split
