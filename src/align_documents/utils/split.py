555386b236524544df6349a8c5be5b4e50f2ed39
Stephan <55782191+iciclejj@users.noreply.github.com>
2025-03-25 14:14:01 +0100

    Update src/align_documents/utils/split.py
    Only allow a PreTrainedTokenizer as tokenizer arg
    
    Co-authored-by: titaenstad <42641610+titaenstad@users.noreply.github.com>

--- src/align_documents/utils/split.py
+++ src/align_documents/utils/split.py
@@ -3,7 +3,7 @@ from semchunk import semchunk
 
 def chunk_texts(
     texts: list[str],
-    tokenizer: str | transformers.PreTrainedTokenizer | tokenizers.Tokenizer,
+    tokenizer: transformers.PreTrainedTokenizer,
     chunk_max_len: int,
 ) -> list[list[str]]:
     """Returns sentences as lists of model_max_len sentence chunks
