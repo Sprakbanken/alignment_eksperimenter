5cc77ca757242a9744fb7b621474e9c3e6c2779e
iciclejj <55782191+iciclejj@users.noreply.github.com>
2025-03-27 15:19:41 +0100

    Allow data_dir from alignment_config.toml in `maalfrid info`

--- src/align_documents/print_overview.py
+++ src/align_documents/print_overview.py
@@ -2,17 +2,12 @@ from pathlib import Path
 import os
 
 def print_overview(args, config):
-    # TODO: Check alignment_config.toml?
-    if args.data_dir:
-        data_dir = args.data_dir
-        if not data_dir.exists():
-            raise ValueError("Data directory does not exist.")
-    else:
-        data_dir = os.environ.get("MALFRID", None)
-        if not data_dir:
-            raise ValueError("No data directory provided.")
-        data_dir = Path(data_dir)
+    data_dir = Path(args.data_dir or os.environ.get("MALFRID", None) or config["data_dir"])
+
+    if not data_dir.exists():
+        raise FileNotFoundError("Provided data directory does not exist")
     
     print(f"Data directory: {data_dir}")
+
     for i, e in enumerate(data_dir.iterdir()):
         print(f"{i}\t{e.name}")
