98d10c0902a42d6c60c93c98f211f2db1b18c46f
theatollersrud <thea.tollersrud@nb.no>
2025-05-28 13:59:56 +0200

    lagt til rapidfuzz i pyproject.toml + leser fra alignment_config

--- scripts/lag_fasit_basert_på_url.py
+++ scripts/lag_fasit_basert_på_url.py
@@ -5,106 +5,116 @@ import re
 from collections import defaultdict
 from rapidfuzz import fuzz
 from concurrent.futures import ThreadPoolExecutor
+from align_documents.utils.config import get_config
 
-data_path_2024 = os.environ.get("MALFRID_2024")
-source_p = Path(data_path_2024)
 
-lang_code_nob = "nob"
-lang_code_nno = "nno"
+if __name__ == "__main__":
+    config = get_config(config_file="alignment_config.toml")
 
-file_group_regex = re.compile(r"(.*?)(?:_nob|_nno)_html")
-lang_code_regex = re.compile(r'/([a-z]{2}-[A-Z]{2}|nynorsk)/')
+    source_p = config.get("data_dir")
+    print("Source path", source_p)
 
-filer = [e for e in source_p.iterdir() if "html" in e.name and (lang_code_nob in e.name or lang_code_nno in e.name)]
+    languages = config.get("languages")
+    print("Languages", languages)
 
-domain_groups = defaultdict(list)
-for file in filer:
-    match = file_group_regex.match(file.stem)
-    if match:
-        domain_groups[match.group(1)].append(file)
-    else:
-        print(f"Regex did not match for file: {file}")
+    lang_code_1= languages[0]
+    lang_code_2 = languages[1]
 
-grouped_files = [
-    (domain, tuple(files))
-    for domain, files in domain_groups.items()
-    if len(files) > 1
-]
-print(f"Grouped domains: {len(grouped_files)}")
+    #file_group_regex = re.compile(r"(.*?)(?:_{lang_code1}|_{lang_code_2})_html")
+    file_group_regex = re.compile(rf"(.*?)(?:_{lang_code_1}|_{lang_code_2})_html")
+    lang_code_regex = re.compile(r'/([a-z]{2}-[A-Z]{2}|nynorsk)/')
 
-def extract_language_code(url):
-    match = lang_code_regex.search(url)
-    return match.group(1) if match else None
+    filer = [e for e in source_p.iterdir() if "html" in e.name and (lang_code_1 in e.name or lang_code_2 in e.name)]
 
-def normalize_url(url):
-    return re.sub(r'/[a-z]{2}-[A-Z]{2}/', '/xx-XX/', url) if extract_language_code(url) else url
+    domain_groups = defaultdict(list)
+    for file in filer:
+        match = file_group_regex.match(file.stem)
+        if match:
+            domain_groups[match.group(1)].append(file)
+        else:
+            print(f"Regex did not match for file: {file}")
 
-def is_valid_url_pair(url1, url2, threshold=97):
-    return fuzz.ratio(normalize_url(url1), normalize_url(url2)) >= threshold
+    grouped_files = [
+        (domain, tuple(files))
+        for domain, files in domain_groups.items()
+        if len(files) > 1
+    ]
+    print(f"Grouped domains: {len(grouped_files)}")
 
-def ends_with_digit(string):
-    match = re.search(r'(\d+)(?=\.html$)', string)
-    return bool(match) if match else False
+    def extract_language_code(url):
+        match = lang_code_regex.search(url)
+        return match.group(1) if match else None
 
-def match_rows(nn_row, df_nob_rows):
-    matches = []
-    nn_url = getattr(nn_row, 'url')
-    for nb_row in df_nob_rows:
-        nb_url = getattr(nb_row, 'url')
-        if is_valid_url_pair(nn_url, nb_url):
-            matches.append({
-                "nynorsk_doc_hash": getattr(nn_row, 'doc_hash'),
-                "bokmaal_doc_hash": getattr(nb_row, 'doc_hash'),
-                "nynorsk_url": nn_url,
-                "bokmaal_url": nb_url,
-                "nynorsk_fulltext": getattr(nn_row, 'fulltext'),
-                "bokmaal_fulltext": getattr(nb_row, 'fulltext')
-            })
-    return matches
+    def normalize_url(url):
+        return re.sub(r'/[a-z]{2}-[A-Z]{2}/', '/xx-XX/', url) if extract_language_code(url) else url
 
-output_dir = Path("output__fasit")
-output_dir.mkdir(exist_ok=True)
+    def is_valid_url_pair(url1, url2, threshold=97):
+        return fuzz.ratio(normalize_url(url1), normalize_url(url2)) >= threshold
 
-def contains_dates_or_many_numbers(url):
-    return (
-    bool(re.search(r'/\d{4}(/|$)', url)) or  #match years
-    bool(re.search(r'\d{6,}', url)) or      #match long sequences of digits
-    bool(re.search(r'/\d+/?$', url)) or       #match a number at the end (e.g 06/)
-    bool(re.search(r'/[^/]*\d+/?$', url))   #match just a digit at the end (e.g 06)
-)
-#process each domain
-for domain, files in grouped_files:
-    print(f"Processing: {domain}")
-    df_nno = pd.DataFrame()
-    df_nob = pd.DataFrame()
+    def ends_with_digit(string):
+        match = re.search(r'(\d+)(?=\.html$)', string)
+        return bool(match) if match else False
 
-    for file in files:
-        if lang_code_nno in file.name:
-            df_nno = pd.read_json(file, lines=True)
-            df_nno = df_nno[~df_nno['url'].apply(contains_dates_or_many_numbers)]
-        elif lang_code_nob in file.name:
-            df_nob = pd.read_json(file, lines=True)
-            df_nob = df_nob[~df_nob['url'].apply(contains_dates_or_many_numbers)]
+    def match_rows(nn_row, df_nob_rows):
+        matches = []
+        nn_url = getattr(nn_row, 'url')
+        for nb_row in df_nob_rows:
+            nb_url = getattr(nb_row, 'url')
+            if is_valid_url_pair(nn_url, nb_url):
+                matches.append({
+                    f"{lang_code_1}_doc_hash": getattr(nn_row, 'doc_hash'),
+                    f"{lang_code_2}_doc_hash": getattr(nb_row, 'doc_hash'),
+                    f"{lang_code_1}_url": nn_url,
+                    f"{lang_code_2}_url": nb_url,
+                    f"{lang_code_1}_fulltext": getattr(nn_row, 'fulltext'),
+                    f"{lang_code_2}_fulltext": getattr(nb_row, 'fulltext')
+                })
+        return matches
 
-    df_nob_rows = list(df_nob.itertuples())
-    all_matches = []
+    output_dir = Path("o")
+    output_dir.mkdir(exist_ok=True)
 
-    with ThreadPoolExecutor() as executor:
-        results = executor.map(lambda row: match_rows(row, df_nob_rows), df_nno.itertuples())
-        for matched_rows in results:
-            all_matches.extend(matched_rows)
+    def contains_dates_or_many_numbers(url):
+        return (
+        bool(re.search(r'/\d{4}(/|$)', url)) or  #match years
+        bool(re.search(r'\d{6,}', url)) or      #match long sequences of digits
+        bool(re.search(r'/\d+/?$', url)) or       #match a number at the end (e.g 06/)
+        bool(re.search(r'/[^/]*\d+/?$', url))   #match just a digit at the end (e.g 06)
+    )
+    #process each domain
+    for domain, files in grouped_files:
+        print(f"Processing: {domain}")
+        df_lang_code_1 = pd.DataFrame()
+        df_lang_code_2 = pd.DataFrame()
 
-    result_df = pd.DataFrame(all_matches)
-    print(f"Matches found for {domain}: {len(result_df)}")
+        for file in files:
+            if lang_code_1 in file.name:
+                df_lang_code_1 = pd.read_json(file, lines=True)
+                df_lang_code_1 = df_lang_code_1[~df_lang_code_1['url'].apply(contains_dates_or_many_numbers)]
+            elif lang_code_2 in file.name:
+                df_lang_code_2 = pd.read_json(file, lines=True)
+                df_lang_code_2 = df_lang_code_2[~df_lang_code_2['url'].apply(contains_dates_or_many_numbers)]
 
-    if len(result_df) > 0:
-        result_df_filtered = result_df[
-        ~result_df['nynorsk_url'].apply(ends_with_digit) &
-        ~result_df['bokmaal_url'].apply(ends_with_digit)
-]
-    else:
-        result_df_filtered = pd.DataFrame()
-    if len(result_df_filtered) > 0:
-        result_df_filtered.to_csv(output_dir / f"{domain}.csv", index=False)
-    else:
-        print(f"No valid matches found for {domain}.")
+        df_lang_code_2_rows = list(df_lang_code_2.itertuples())
+        all_matches = []
+
+        with ThreadPoolExecutor() as executor:
+            results = executor.map(lambda row: match_rows(row, df_lang_code_2_rows), df_lang_code_1.itertuples())
+            for matched_rows in results:
+                all_matches.extend(matched_rows)
+
+        result_df = pd.DataFrame(all_matches)
+        print(f"Matches found for {domain}: {len(result_df)}")
+
+        if len(result_df) > 0:
+            result_df_filtered = result_df[
+            ~result_df[f"{lang_code_1}_url"].apply(ends_with_digit) &
+            ~result_df[f"{lang_code_2}_url"].apply(ends_with_digit)
+    ]
+        else:
+            result_df_filtered = pd.DataFrame()
+        if len(result_df_filtered) > 0:
+            result_df_filtered.to_csv(output_dir / f"{domain}.csv", index=False)
+        else:
+            print(f"No valid matches found for {domain}.")
+    
