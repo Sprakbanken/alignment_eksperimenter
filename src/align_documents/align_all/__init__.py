ef42277a18ba1369f5916953528f6d646e66ed52
Tita <42641610+titaenstad@users.noreply.github.com>
2025-09-24 13:42:30 +0200

    Reorganize utils imports (#67)
    * Code quality: Move dataframe filtering to utils.dataframe and use pandas.Dataframe.reset_index instead on manually setting index
    
    * Add negative doc pairs arguments as cli args instead of hard-coding
    
    * Cleanup find_negative_doc_pairs and type config
    
    * Use dataclass for config and make validation_and_cast a class method
    
    * Add some debug logs
    
    * Remove old negative pairs
    
    * Restructure align code for alignment comparison
    
    * Create from_dict class method in Config class
    
    * Add info and debug logs to script
    
    * pandas indexing bugfix, output_dir subdirs for align and negative_pairs, and update readme
    
    * Use typing.Self as return value of Config.from_dict
    
    * Move setup_logging from utils.logging to utils to avoid circular import
    
    * Remove unused code
    
    * Import single-function submodules of utils functions to outer utils module and update imports accordingly
    
    * Move setup_logging import to config main method to avoid circular imports

--- src/align_documents/align_all/__init__.py
+++ src/align_documents/align_all/__init__.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import logging
 from tqdm import tqdm
-from align_documents.utils.get_embedding_model import get_embedding_model
-from align_documents.utils.config import get_config
-from align_documents.utils import setup_logging
+from align_documents.utils import setup_logging, get_config, get_embedding_model
 from align_documents.utils.dataframe import (
     get_file_info,
     get_websites_with_both_langs,
