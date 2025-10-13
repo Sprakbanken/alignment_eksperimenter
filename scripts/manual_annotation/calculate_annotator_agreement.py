10c0e005f50b16f20b2e29f7a095ab90d78f177c
Tita <tita.enstad@nb.no>
2025-10-13 15:01:17 +0200

    Add script to calculate inter-annotator agreement

---
+++ scripts/manual_annotation/calculate_annotator_agreement.py
@@ -0,0 +1,83 @@
+import argparse
+import logging
+from pathlib import Path
+from align_documents.utils import setup_logging
+from sklearn.metrics import cohen_kappa_score
+import pandas as pd
+
+
+logger = logging.getLogger(__name__)
+
+
+def add_lang1_lang2_hash(df: pd.DataFrame) -> pd.DataFrame:
+    df["doc_hash"] = df.doc_hash_lang_1 + df.doc_hash_lang_2
+    return df
+
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Calculate inter-annotator agreement for manual annotations"
+    )
+    parser.add_argument(
+        "--annotator_one",
+        type=Path,
+        default=Path("manual_annotation/annotated_data/annotator_1.csv"),
+        help="Path to the annotated data (.csv-file) from annotator 1 (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--annotator_two",
+        type=Path,
+        default=Path("manual_annotation/annotated_data/annotator_2.csv"),
+        help="Path to the annotated data (.csv-file) from annotator 2 (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--log_level",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Logging level (default: %(default)s)",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    setup_logging("calculate_annotator_agreement", args.log_level)
+
+    df1 = pd.read_csv(args.annotator_one)
+    df2 = pd.read_csv(args.annotator_two)
+    logger.debug("len(df1): %s" % len(df1))
+    logger.debug("len(df2): %s" % len(df2))
+
+    labels = df1.annotation.unique()
+    logger.debug("labels: %s", labels)
+
+    # Concatenate doc hashes for both documents in annotated doc pair
+    df1 = add_lang1_lang2_hash(df1)
+    df2 = add_lang1_lang2_hash(df2)
+
+    # Deduplicate
+    df1 = df1.drop_duplicates("doc_hash")
+    logger.debug("len(df1) after deduplication: %s", len(df1))
+    df2 = df2.drop_duplicates("doc_hash")
+    logger.debug("len(df2) after deduplication: %s", len(df2))
+
+    # Only keep document pairs that are annotated by both annotators
+    overlapping_doc_hashes = set(df1.doc_hash).intersection(set(df2.doc_hash))
+    logger.info("%s document pairs are doubly annotated", len(overlapping_doc_hashes))
+
+    df1 = df1[df1.doc_hash.isin(overlapping_doc_hashes)]
+    logger.debug("len(df1) %s", len(df1))
+    df2 = df2[df2.doc_hash.isin(overlapping_doc_hashes)]
+    logger.debug("len(df2) %s", len(df2))
+
+    score = cohen_kappa_score(
+        y1=sorted(df1.annotation), y2=sorted(df2.annotation), labels=labels
+    )
+
+    logger.info("Cohens kappa: %s", score)
+
+
+if __name__ == "__main__":
+    main()
